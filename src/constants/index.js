6d737d3b6c376b8f4b38f1f3ab5b1929688712b5
swarnavopramanik <105142693+swarnavopramanik@users.noreply.github.com>
2023-04-04 20:27:16 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,328 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  Aicte,
+  clause,
+  devtown,
+  lets,
+  oasis,
+  swoc,
+  sync,
+  talent,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Full Stack Developer",
+    icon: web,
+  },
+  {
+    title: "React Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "AI,ML Developer",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Full-Stack Developer Trainee",
+    company_name: "DevTown",
+    icon: devtown,
+    iconBg: "#383E56",
+    date: "Jun 2022 - Nov 2022",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Completed Machine Learning Project Challenge",
+    company_name: "Talent Battle",
+    icon: talent,
+    iconBg: "#E6DEDD",
+    date: "Dec 2022 - Feb 2022",
+    points: [
+      "Developing and maintaining Machine learning Project and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing machine learning model and build In image recognition, a machine learning model .",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Artificial Intelligence intership ",
+    company_name: "AICTE NEAT ",
+    icon: Aicte,
+    iconBg: "#383E56",
+    date: "Jan 2022 - Feb 2022",
+    points: [
+      "Developing and maintaining Machine learning , AI  Project and learn more concept other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Web Development Intern",
+    company_name: "CodeClause",
+    icon: clause,
+    iconBg: "#E6DEDD",
+    date: "Jan 2022 - Feb 2022",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  
+  {
+    title: "Web Development and Designing Intern ",
+    company_name: "Oasis Infobytee",
+    icon: oasis,
+    iconBg: "#E6DEDD",
+    date: "Jan 2022 - Feb 2022",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Participate in Social Winter of Code S3 Hackathon ",
+    company_name: "Social Winter of Code",
+    icon: swoc,
+    iconBg: "#E6DEDD",
+    date: "Jan 2022 - Mar 2022",
+    points: [
+      "Participate as a Project Admin and building a FULL STACK PROJECT ( MERN STACK).",
+      "Social Winter of Code aims to bring students into the world of open source development and see the power of unified problem-solving in real time. The projects that we will host have been carefully hand-picked to invigorate creative thinking and encourage collaboration among all participants.",
+      "Provided Project admin . (Open Source).",
+      "I am  glad to share my Open Source Project Task Management Application  is participating in Social Winter of Code and My Project and It is available official site of Social Winter of Code my project are .",
+    ],
+    
+  },
+
+  {
+    title: "Reach.Js  Intern",
+    company_name: "SYNC INTERN'S",
+    icon:  sync,
+    iconBg: "#E6DEDD",
+    date: "Feb 2022 - Mar 2022",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+
+  {
+    title: "Web Development Intern",
+    company_name: "LetsGrowMore",
+    icon: lets,
+    iconBg: "#E6DEDD",
+    date: "April 2022 - Present",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+
+  
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Disha Roy",
+    designation: "CFO",
+    company: "Acme Co",
+    
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Tanmoy Pal",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Shreya Roy",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "MERN Stack Food Delivary App",
+    description:
+      "MERN Food Ordering System is a Web app built on the MERN (MongoDB, Express, React, Node) stack of web development. It uses Redux for the state management and Material UI as component library. It is a food ordering website, where registered users can order for food from a particular restaurant.",
+    tags: [
+      {
+        name: "React, Node , Mongodb",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind CSS",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "React Admin Dashboard ",
+    description:
+      "In today’s world, every online program interface (mainly e-commerce platforms) contains a page where information and data about customers, clients, and consumers is shown or monitored. This information/data could be based on purchases, financial transactions, or consumer service statistics. An Admin Dashboard is a visual representation of your whole data set. While it can be used in various ways, its primary function is to allow quick access to information, such as APIs. An Admin Dashboard is often shown on its own website and receives data from a linked database. So, today, we’ll develop a responsive and interactive Admin Dashboard using the well-known React JavaScript framework, plus the Material UI and Rechart libraries .",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "3D Customization Threejs AI Website",
+    description:
+      "Many large corporations already use 3D graphics to showcase their products. ThreeJS 3D product website and infuse it with the power of artificial intelligence! , Load, create and customize stunning 3D models and geometries with various lights, as well as understand the 3D world with a camera and positioning of an object in space. Make your code reusable and scalable using Higher Order Components (HOCs) and other industry-standard best practices Add custom color and file support. Generate and use images through DALLE AI Download the resulting t-shirt model image Ensure responsiveness across all devices and improve your site's performance",
+    tags: [
+      {
+        name: "Threejs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Reactjs",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
